c07f645842b274294e002b8074c60772bc6a1987
mide <37365432+michaeldeutsch@users.noreply.github.com>
2026-03-23 12:05:05 +0100

    move ForSchleife to schleife package

---
+++ src/main/java/einheit02/schleife/EinMalEins.java
@@ -0,0 +1,22 @@
+package einheit02.schleife;
+
+public class EinMalEins {
+
+    static void main() {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/einheit02/schleife/Entenhausen.java
@@ -0,0 +1,23 @@
+package einheit02.schleife;
+
+public class Entenhausen {
+
+    static void main() {
+
+        int geld = 500;
+
+        // trick track tick, jeder bekommt etwas, track bekommt eine gerade Summe,
+        // tick bekommt mehr als trick, und keiner bekommt mehr als die Hälfe
+
+        for (int trick = 0; trick < geld; trick++) {
+            for (int track = 0; track < geld; track++) {
+                for (int tick = 0; tick < geld; tick++) {
+
+                }
+            }
+        }
+
+
+
+    }
+}
