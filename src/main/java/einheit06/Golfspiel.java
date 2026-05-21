b1af07f753b40259b28b0fea672443139178ef34
mide <37365432+michaeldeutsch@users.noreply.github.com>
2026-05-21 10:03:29 +0200

    Fertigstellung: Implementiere Spielwertung, Gewinnerermittlung und Spieler-Score-Anzeige für Golf

--- src/main/java/einheit06/Golfspiel.java
+++ src/main/java/einheit06/Golfspiel.java
@@ -1,5 +1,6 @@
 package einheit06;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,6 +15,7 @@ public class Golfspiel {
         int[][] golfspiel = new int[anzahlSpieler][anzahlLoecher];
         String[] namen = new String[anzahlSpieler];
         int[] wahrscheinlichkeit = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+        int[] gesamtScoreBySpieler = new int[anzahlSpieler];
 
         // Einlesen - Anfang
         for (int i = 0; i < anzahlSpieler; i++) {
@@ -27,17 +29,41 @@ public class Golfspiel {
         // Spiel beginnt
         //  < -  <-
         for (int spieler = 0; spieler < anzahlSpieler; spieler++) {
+
+            int sum = 0;
             for (int loch = 0; loch < anzahlLoecher; loch++) {
                 // golfspiel[spieler][loch] = ThreadLocalRandom.current().nextInt(1, 4 + 1); // gleichvereilt
                 int index = ThreadLocalRandom.current().nextInt(0, 9 + 1); // Häufig lt. Angabe
                 golfspiel[spieler][loch] = wahrscheinlichkeit[index]; // {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+                sum = sum + golfspiel[spieler][loch];
 
             }
+            gesamtScoreBySpieler[spieler] = sum; // 234
         }
+
+        System.out.println(Arrays.toString(gesamtScoreBySpieler));
 
         print2DArray(anzahlSpieler, anzahlLoecher, golfspiel);
 
+        // Wertung - Anfang
+        int minScore = 4 * anzahlLoecher;
+        int indexMinScore = -1;
 
+        for (int spieler = 0; spieler < anzahlSpieler; spieler++) {
+            if (gesamtScoreBySpieler[spieler] <= minScore) { // 60 < 72
+                minScore = gesamtScoreBySpieler[spieler]; // 60
+                indexMinScore = spieler;
+            }
+        }
+
+
+        // Wertung - Ende
+
+
+        // Gewinner verlautbaren - Anfang
+        System.out.println("Der Gewinner ist  " + namen[indexMinScore] + " mit einer Gesamtschalgzahl von " + gesamtScoreBySpieler[indexMinScore]);
+
+        // Gewinner verlautbaren - Ende
         // Spiel - Ende
 
     }
